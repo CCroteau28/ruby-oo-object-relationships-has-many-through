c505625e4eed0d5a7da5bbd933444a0c78665656
CCroteau28 <ccroteau28@gmail.com>
2021-03-21 17:52:52 -0600

    commit tree 561cd8e7b2e4abb4ef5ac996be38c6ff71833137

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class Customer
-  
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.customer == self}
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def waiters
+        meals.map do |meal|
+          meal.waiter
+        end
+    end
 end
